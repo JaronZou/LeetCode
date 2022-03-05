589b658042a6f90e44ea7a583e9a032bf7d3babe
JaronZou <JaronZou@outlook.com>
2022-03-05 21:59:32 +0800

    更新了md

--- code/32.最长有效括号.cpp
+++ code/32.最长有效括号.cpp
@@ -18,13 +18,14 @@ public:
         //      s[i - dp[i - 1] - 1] == '('
         //          dp[i] = dp[i - 1] + 2 + dp[i - dp[i - 1] - 2]
         //          前提是上面的下标都不越界
-        //      越界或s[i - dp[i - 1] - 1] != '('
+        //      下标发生越界或s[i - dp[i - 1] - 1] != '('
         //          dp[i] = 0
         vector<int> dp(s.size(), 0);
         int maxLen = 0;
         for(int i = 1; i < s.size(); ++i) {
             if(s[i] == ')') {
                 int index = i - dp[i - 1] - 1;
+                // index未发生越界且有s[index] == '(' && s[i] == ')'
                 if(index >= 0 && s[index] == '(') {
                     dp[i] = dp[i - 1] + 2;
                     // 防止index - 1越界
