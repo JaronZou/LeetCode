af252b5f7335476c797d96b03ce15b42c1dae6b4
JaronZou <JaronZou@outlook.com>
2022-03-07 11:29:02 +0800

    删除倒是第n个结点 done

--- code/19.删除链表的倒数第-n-个结点.cpp
+++ code/19.删除链表的倒数第-n-个结点.cpp
@@ -1,4 +1,4 @@
-/*
+fou/*
  * @lc app=leetcode.cn id=19 lang=cpp
  *
  * [19] 删除链表的倒数第 N 个结点
@@ -17,8 +17,58 @@
  */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
+    // ListNode* removeNthFromEnd(ListNode* head, int n) {
+    //     int count = 0;
+    //     ListNode *cur = head;
+    //     while(cur != nullptr) {
+    //         ++count;
+    //         cur = cur->next;
+    //     }
+    //     if(count == n) {
+    //         cur = head;
+    //         head = head->next;
+    //         delete cur;
+    //         return head;
+    //     }
+    //     else {
+    //         cur = head;
+    //         count = count - n;
+    //         ListNode *pre, *next;
+    //         while(count != 0) {
+    //             --count;
+    //             pre = cur;
+    //             cur = cur->next;
+    //         }
+    //         next = cur->next;
+    //         pre->next = next;
+    //         delete cur;
+    //     }
+    //     return head;
+    // }
 
+    // 双指针
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *slow = head, *fast = head;
+        while(n != 0) {
+            --n;
+            fast = fast->next;
+        }
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        // fast == nullptr，那么要删除的就是头结点
+        if(fast == nullptr) {
+            head = head->next;
+            delete slow;
+        }
+        // 否则，删除的就是中间的某个结点
+        else {
+            ListNode *cur = slow->next;
+            slow->next = cur->next;
+            delete cur;
+        }
+        return head;
     }
 };
 // @lc code=end
